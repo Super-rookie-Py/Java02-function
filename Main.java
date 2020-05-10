ac158561300f1355efea7196b1459b71e1325e99
Super-rookie-Py <54101813+Super-rookie-Py@users.noreply.github.com>
2020-05-10 23:57:29 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+package function;
+
+public class Main {
+	
+	final static double PI = 3.141592; // ¹Ù²ð ¼ö ¾ø´Â°ª.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intType = 100;
+		double doubleType = 150.5;
+		String stringType = "KeonwooPark";
+		
+		System.out.println(intType); // ÇÑÄ­ ÁÙ¹Ù²ÞÇÏ±â. println
+		System.out.println(doubleType);
+		System.out.println(stringType);
+		
+		int r = 30;
+		System.out.println(r*r*PI); // ¿ø ³ÐÀÌ ±¸ÇÏ±â
+
+	}
+
+}
